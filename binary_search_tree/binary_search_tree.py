0f2eddbaf3b30221f752c293df14fc752b4d0c57
Savannah Greuel <smallidgesavannah@gmail.com>
2019-04-03 12:01:47 -0600

    bst complete

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,13 +5,54 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    # adds the input value to the binary search tree, adhering to the rules of the ordering of elements in a binary search tree.
+    root = self
+    given = value
+    complete = False
+    while not complete:
+      if given < root.value:
+        if root.left:
+          root = root.left
+        else:
+          root.left = BinarySearchTree(given)
+          complete = True
+
+      if given > root.value:
+        if root.right:
+          root = root.right
+        else:
+          root.right = BinarySearchTree(given)
+          complete = True
 
   def contains(self, target):
-    pass
+    #searches the binary search tree for the input value, returning a boolean indicating whether the value exists in the tree or not.
+    root = self
+    complete= False
+    while not complete:
+      if not root:
+        return False
+      if root.value == target:
+        return True
+      elif root.value < target:
+        root = root.right
+      else:
+        root = root.left
 
   def get_max(self):
-    pass
+    #  returns max val in bst
+    current = self
+    max = 0
+    
+    while current:
+      if current.value > max:
+        max = current.value
+        current = current.right
+    return max
+    
 
   def for_each(self, cb):
-    pass
+    cb(self.value)
+    if self.left != None:
+      self.left.for_each(cb)
+    if self.right != None:
+      self.right.for_each(cb)
