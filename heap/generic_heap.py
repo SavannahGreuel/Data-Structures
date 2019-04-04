f2245e962e5d3cee36d7214909e495d3de77fedf
Savannah Greuel <smallidgesavannah@gmail.com>
2019-04-03 20:43:03 -0600

    max_heap is working, passes all tests

--- heap/generic_heap.py
+++ heap/generic_heap.py
@@ -4,19 +4,37 @@ class Heap:
     self.comparator = comparator
 
   def insert(self, value):
-    pass
+    self.storage.append(value)
+    self._bubble_up(len(self.storage) - 1)
 
   def delete(self):
-    pass
+    removed_item = self.storage[0]
+    del self.storage[0]
+    self._sift_down(0)
+    return removed_item
 
   def get_priority(self):
-    pass
+    return self.storage[0]
 
   def get_size(self):
-    pass
+    return len(self.storage)
 
   def _bubble_up(self, index):
-    pass
-
+    parent = (index - 1) // 2
+    if index <= 0:
+      return
+    elif self.storage[index] > self.storage[parent]:
+      self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+      self._bubble_up(parent)
+  
   def _sift_down(self, index):
-    pass
+     while index * 2 + 1 <= len(self.storage) - 1:
+      if index * 2 + 2 > len(self.storage) - 1:
+        maxSize = index * 2 + 1
+      elif self.storage[index * 2 + 1] > self.storage[index * 2 + 2]:
+        maxSize = index * 2 + 1
+      else:
+        maxSize = index * 2 + 2
+      if self.storage[maxSize] > self.storage[index]:
+        self.storage[maxSize], self.storage[index] = self.storage[index], self.storage[maxSize]
+      index = maxSize
